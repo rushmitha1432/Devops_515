db3472ff62cc8bfcff8ecbf4e82f980a31692417
rushm <rushm@Rushmitha>
2025-09-03 15:50:26 +0530

    Hai

--- Devops_515/src/Lbrce_Devops/HelloWorld.java
+++ Devops_515/src/Lbrce_Devops/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Word");
 		System.out.println("rushmitha");
+		System.out.println("Lakshmi");
 	}
 
 }
